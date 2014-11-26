09ebc46ef49e13f6be97870fd7e846aae278dcb0
karan <jilkakaran@gmail.com>
2014-11-27 00:32:50 +0530

    some formate fixes

--- example/getdata.php
+++ example/getdata.php
@@ -12,7 +12,6 @@ $password = '123456';
 /* * * mysql password ** */
 $db = 'datafreedom';
 
-
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$db", $username, $password);
     /*     * * echo a message saying we have connected ** */
@@ -22,33 +21,33 @@ try {
 
 $out = array();
 
-$out['current_page']=(!empty($_GET['page']))?(int) $_GET['page']:1;
-$out['per_page']=(!empty($_GET['limit']))?(int) $_GET['limit']:10;
+$out['current_page'] = (!empty($_GET['page'])) ? (int) $_GET['page'] : 1;
+$out['per_page'] = (!empty($_GET['limit'])) ? (int) $_GET['limit'] : 10;
 
-$offset = (($out['current_page']-1)*$out['per_page']);
-$out['from']=$offset+1;
-$out['to']=$out['from']+($out['per_page']-1);
+$offset = (($out['current_page']-1) * $out['per_page']);
+$out['from'] = $offset + 1;
+$out['to'] = $out['from']+($out['per_page']-1);
 
 $where = " WHERE 1=1";
-if(!empty($_POST['search']['name'])){
-    $where .= " AND name like '%".$_POST['search']['name']."%'";
+if (!empty($_POST['search']['name'])) {
+    $where .= " AND name like '%" . $_POST['search']['name'] . "%'";
 }
 
-if(!empty($_POST['sort'])){
-    foreach($_POST['sort'] as $key => $val){
-        $where .= " ORDER BY ".$key.' '.$val;
+if (!empty($_POST['sort'])) {
+    foreach ($_POST['sort'] as $key => $val) {
+        $where .= " ORDER BY " . $key . ' ' . $val;
     }
 }
 
 //get total rows
-$sql = "SELECT count(*) FROM dummy_data".$where;
+$sql = "SELECT count(*) FROM dummy_data" . $where;
 $total = $dbh->query($sql)->fetchColumn(0);
-$out['total']=(int) $total;
-$out['last_page']=ceil($out['total']/$out['per_page']);
+$out['total'] = (int) $total;
+$out['last_page'] = ceil($out['total'] / $out['per_page']);
 
 /* * * The SQL SELECT statement ** */
-$where .= " LIMIT ".$offset.",".$out['per_page'];
-$sql = "SELECT * FROM dummy_data".$where;
+$where .= " LIMIT " . $offset . "," . $out['per_page'];
+$sql = "SELECT * FROM dummy_data" . $where;
 
 $out['data'] = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
--- js/app.js
+++ js/app.js
@@ -1,55 +1,45 @@
-(function () {
+(function() {
     var app = angular.module("demo", ['dataFreedom']);
-
-    app.controller("demoCtrl", ['$scope','dataFreedomService', function ($scope,dataFreedomService) {
-
-        dataFreedomService.method = 'POST';
-        $scope.sort = {};
-        $scope.sort['id']='asc';
-        dataFreedomService.sort=$scope.sort;
-
-        $scope.fetch=function(){
-             dataFreedomService.fetch().then(function(data){
-                $scope.data = data.data;
-                $scope.total = data.total;
-                $scope.totalItems = data.total;
-                $scope.bigTotalItems = data.total;
-                $scope.numPages = data.last_page;
-
-                $scope.maxSize = data.per_page;
-
-                $scope.currentPage = data.current_page;
-                $scope.bigCurrentPage = data.current_page;
-            },function(data){
-                alert(data);
-            });
-        };
-
-        $scope.pageChanged = function () {
-            dataFreedomService.bigCurrentPage = $scope.bigCurrentPage;            
+    app.controller("demoCtrl", ['$scope', 'dataFreedomService',
+        function($scope, dataFreedomService) {
+            dataFreedomService.method = 'POST';
+            $scope.sort = {};
+            $scope.sort['id'] = 'asc';
+            dataFreedomService.sort = $scope.sort;
+            $scope.fetch = function() {
+                dataFreedomService.fetch().then(function(data) {
+                    $scope.data = data.data;
+                    $scope.total = data.total;
+                    $scope.totalItems = data.total;
+                    $scope.bigTotalItems = data.total;
+                    $scope.numPages = data.last_page;
+                    $scope.maxSize = data.per_page;
+                    $scope.currentPage = data.current_page;
+                    $scope.bigCurrentPage = data.current_page;
+                }, function(data) {
+                    alert(data);
+                });
+            };
+            $scope.pageChanged = function() {
+                dataFreedomService.bigCurrentPage = $scope.bigCurrentPage;
+                $scope.fetch();
+            };
+            $scope.search = function(find) {
+                dataFreedomService.search = find;
+                $scope.fetch();
+            };
+            $scope.sorting = function(field) {
+                if ($scope.sort[field] && $scope.sort[field] === 'asc') {
+                    $scope.sort = {};
+                    $scope.sort[field] = 'desc';
+                } else {
+                    $scope.sort = {};
+                    $scope.sort[field] = 'asc';
+                }
+                dataFreedomService.sort = $scope.sort;
+                $scope.fetch();
+            };
             $scope.fetch();
-        };
-        
-        $scope.search = function (find) {            
-            dataFreedomService.search = find;                       
-            $scope.fetch();
-        };
-
-        $scope.sorting = function (field) {            
-            if($scope.sort[field] && $scope.sort[field]==='asc'){
-                $scope.sort = {};
-                $scope.sort[field]='desc';
-            }else{
-                $scope.sort = {};
-                $scope.sort[field]='asc';
-            }
-                                 
-            dataFreedomService.sort=$scope.sort;                      
-            $scope.fetch();
-        };
-
-        $scope.fetch();
-
-    }]);
-
+        }
+    ]);
 })();
